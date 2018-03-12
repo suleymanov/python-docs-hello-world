fae9e218f0872d030a6a0b54e9f14df050ba9ce4
suleymanov <rail.suleymanov@gmail.com>
2018-03-12 14:06:21 +0300

    Update reqs and import pymongo, but no init

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from pymongo import MongoClient
+
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
+
 
 if __name__ == '__main__':
   app.run()
