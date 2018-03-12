d8dc43b32569394daff2cd3df00c4884eef73cd1
suleymanov <rail.suleymanov@gmail.com>
2018-03-12 14:07:26 +0300

    init now

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 
 
 app = Flask(__name__)
+client = MongoClient('mongodb://macrodb:umQucbmm86HkVJSbq7JwBMbsalBn4nuX2WEO5cc171LyudHCAG4ixa1FeS00ssm4JO6V69dy850pP9qWrccpnA==@macrodb.documents.azure.com:10255/?ssl=true&replicaSet=globaldb')
 
 
 @app.route('/')
